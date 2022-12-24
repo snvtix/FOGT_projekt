8028cef2f8ad5e23fb0b1910312dbc231d2a9e73
snvtix <natslep17@onet.pl>
2022-12-24 22:26:18 +0100

    Przykładowy wykres 3D

--- fogt.py
+++ fogt.py
@@ -1,35 +1,17 @@
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import numpy as np
 
-# ustawiamy wymiary pudełka
-Lx = 10
-Ly = 10
-Lz = 10
+X, Y, Z = np.mgrid[-8:8:40j, -8:8:40j, -8:8:40j]
+values = np.sin(X*Y*Z) / (X*Y*Z)
 
-# tworzymy siatkę dla współrzędnych
-x = np.linspace(0, Lx, 100)
-y = np.linspace(0, Ly, 100)
-z = np.linspace(0, Lz, 100)
-
-# obliczamy wartość funkcji w każdym punkcie na siatce
-V = np.zeros((100, 100, 100))
-for i in range(100):
-    for j in range(100):
-        for k in range(100):
-            V[i,j,k] = x[i]**2 + y[j]**2 + z[k]**2
-
-# rysujemy wykres przestrzeni stanów
-plt.figure()
-plt.imshow(V[:,:,0], cmap='viridis')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Przestrzeń stanów')
-
-# rysujemy wykres wartości funkcji
-plt.figure()
-plt.imshow(V[:,:,0], cmap='viridis')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Wartość funkcji')
-
-plt.show()
+fig = go.Figure(data=go.Volume(
+    x=X.flatten(),
+    y=Y.flatten(),
+    z=Z.flatten(),
+    value=values.flatten(),
+    isomin=0.1,
+    isomax=0.8,
+    opacity=0.1, # needs to be small to see through all surfaces
+    surface_count=17, # needs to be a large number for good volume rendering
+    ))
+fig.show()
